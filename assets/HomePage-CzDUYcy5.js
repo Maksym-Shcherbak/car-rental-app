7b0c80d74eb11cbe06b78c0eff427c0dc5da03c6
Maksym-Shcherbak <130525995+Maksym-Shcherbak@users.noreply.github.com>
2024-03-10 21:27:47 +0000

    Deploying to gh-pages from @ Maksym-Shcherbak/car-rental-app@74034ab0f616eff835edee2f4fcee5826d0bf284 🚀

--- assets/HomePage-CzDUYcy5.js
+++ assets/HomePage-CzDUYcy5.js
@@ -1,4 +1,4 @@
-import{q as o,L as t,j as a}from"./index--5bYL8Uv.js";const n=o.div`
+import{q as o,L as t,j as a}from"./index-DTvOm0Nt.js";const n=o.div`
   max-width: 800px;
   padding: 21px 10px;
   text-align: left;
